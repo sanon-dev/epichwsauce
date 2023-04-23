d87495f48445b0b230aa076009786ccd1f94c6a2
yifei-yao <yaoyifei0@gmail.com>
2023-04-23 17:38:59 -0400

    Add document picker
    Need to fix issues

--- project/epic-hw-sauce-frontend/components/scan_upload.js
+++ project/epic-hw-sauce-frontend/components/scan_upload.js
@@ -1,18 +1,56 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Button } from 'react-native-paper';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import { styles } from '../styles.js';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Button } from "react-native-paper";
+import Icon from "react-native-vector-icons/FontAwesome5";
+import { styles } from "../styles.js";
+import * as DocumentPicker from "react-native-document-picker";
+
 function ScanUploadScreen() {
+  const handleFilePick = async () => {
+    try {
+      const result = await DocumentPicker.pick({
+        type: [DocumentPicker.types.allFiles],
+      });
+      console.log(
+        result.uri,
+        result.type, // mime type
+        result.name,
+        result.size
+      );
+    } catch (err) {
+      if (DocumentPicker.isCancel(err)) {
+        // User cancelled the picker
+      } else {
+        // Error!
+        console.log(err);
+      }
+    }
+  };
   return (
-    <View style={[styles.container, styles.centerContainer, styles.scanUploadContainer]}>
+    <View
+      style={[
+        styles.container,
+        styles.centerContainer,
+        styles.scanUploadContainer,
+      ]}
+    >
       <Text style={styles.heading}>Scan & Upload</Text>
       <View style={styles.buttonContainer}>
-        <Button mode="contained" color="#2196F3" style={styles.button} onPress={() => console.log('Pressed scan')}>
+        <Button
+          mode="contained"
+          color="#2196F3"
+          style={styles.button}
+          onPress={() => console.log("Pressed scan")}
+        >
           <Icon name="camera" size={20} color="white" />
           <Text style={styles.buttonText}>Scan</Text>
         </Button>
-        <Button mode="contained" color="#F44336" style={styles.button} onPress={() => console.log('Pressed upload')}>
+        <Button
+          mode="contained"
+          color="#F44336"
+          style={styles.button}
+          onPress={handleFilePick}
+        >
           <Icon name="file-upload" size={20} color="white" />
           <Text style={styles.buttonText}>Upload</Text>
         </Button>
