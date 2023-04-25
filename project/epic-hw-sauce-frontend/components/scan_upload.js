e5337443e0a65d611a4348f87eeb50e9df3864b9
yifei-yao <yaoyifei0@gmail.com>
2023-04-25 10:51:14 -0400

    Fix document picking

--- project/epic-hw-sauce-frontend/components/scan_upload.js
+++ project/epic-hw-sauce-frontend/components/scan_upload.js
@@ -1,29 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Button } from "react-native-paper";
 import Icon from "react-native-vector-icons/FontAwesome5";
 import { styles } from "../styles.js";
-import * as DocumentPicker from "react-native-document-picker";
+import * as DocumentPicker from "expo-document-picker";
 
 function ScanUploadScreen() {
-  const handleFilePick = async () => {
+  const [fileUri, setFileUri] = useState(null);
+
+  const pickDocument = async () => {
     try {
-      const result = await DocumentPicker.pick({
-        type: [DocumentPicker.types.allFiles],
-      });
-      console.log(
-        result.uri,
-        result.type, // mime type
-        result.name,
-        result.size
-      );
-    } catch (err) {
-      if (DocumentPicker.isCancel(err)) {
-        // User cancelled the picker
+      const result = await DocumentPicker.getDocumentAsync();
+      if (result.type === 'success') {
+        console.log(
+          result.uri,
+          result.type, // mime type
+          result.name,
+          result.size
+        );
+        setFileUri(result.uri);
       } else {
-        // Error!
-        console.log(err);
+        console.log('Document selection cancelled');
       }
+    } catch (err) {
+      console.log('Document selection error:', err);
     }
   };
   return (
@@ -49,7 +49,7 @@ function ScanUploadScreen() {
           mode="contained"
           color="#F44336"
           style={styles.button}
-          onPress={handleFilePick}
+          onPress={pickDocument}
         >
           <Icon name="file-upload" size={20} color="white" />
           <Text style={styles.buttonText}>Upload</Text>
